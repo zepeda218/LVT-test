e81aac3fb01d37a6bc011471126540ba8a468eaa
zepeda218 <carlazepeda218@gmail.com>
2020-11-19 21:44:34 -0600

    Responsivness in  result page added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,21 @@
-$.ajax({
-    dataType: "json",
-    url: 'https://ltv-data-api.herokuapp.com/api/v1/records.json',
-    data: data,
-    success: success
+$(document).ready(function () {
+  $(".btn-search").on("click", function (e) {
+    email = $('input[type="search"]').val();
+    $.ajax({
+      type: "GET",
+      crossDomain: true,
+      url:
+        "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email + '.',
+      dataType: "jsonp",
+      success: function (data) {
+        //if the request is successful do something with the data
+        alert(data);
+      },
+      error: function (request) {
+        //if the request fails, log what happened
+        console.log(request);
+        alert(JSON.stringify("Error: " + request));
+      },
+    });
   });
+});
