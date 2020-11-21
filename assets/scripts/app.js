ad95b1fb42fe9cb1bb8f84e21c5174c557e2d611
zepeda218 <carlazepeda218@gmail.com>
2020-11-21 16:37:51 -0600

    Email validations added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,32 @@
 $(document).ready(function () {
-
-    $(".btn-search").on("click", function (e) {
-        localStorage.clear();
-        email = $('input[type="search"]').val();
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email; // site that doesn’t send Access-Control-*
-        fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-            .then(response => response.text())
-            .then(function (contents) {
-                // if (contents == '[]') {
-                //     console.log(contents)
-                //     return;
-                // } else {
-                    console.log(contents);
-                    localStorage.setItem('userObject', contents);
-                    window.location.href = "result.html"; 
-                // }
-            })
-            .catch((e) => console.log(e))
-    });
+  $(".btn-search").on("click", function (e) {
+    localStorage.clear();
+    email = $('input[type="text"]').val();
+    if (validateEmail(email)) {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url =
+        "https://ltv-data-api.herokuapp.com/api/v1/records.json?email=" + email;
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          console.log(contents);
+          localStorage.setItem("userObject", contents);
+          window.location.href = "result.html";
+        })
+        .catch((e) => console.log(e));
+    } else {
+      $('input[type="text"]').addClass("error");
+      $('span.floating-label').addClass("error");
+      $(".floating-label").text("Please add a valid email address");
+    }
+  });
 });
 
-
+function validateEmail(email) {
+  regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  if (email.match(regEx)) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -1,28 +1,34 @@
 $(document).ready(function () {
-    setContent()
+    setContent();
 });
 
-
 function setContent() {
-    if (window.localStorage && localStorage.userObject) {
-        userObject = localStorage.getItem("userObject");
-        retreivedObject = JSON.parse(userObject);
-        if (JSON.stringify(retreivedObject) == '[]') {
-            $(".result-card").hide();
-            $(".result-desc").text('No results found.');
-        } else {
-            $(".username").append(retreivedObject.first_name + " " + retreivedObject.last_name)
-            $(".user-description").append(retreivedObject.description)
-            $(".address").append("<p>" + retreivedObject.address + "</p>")
-            $(".email").append("<p>" + retreivedObject.email + "</p>")
+  if (window.localStorage && localStorage.userObject) {
+    userObject = localStorage.getItem("userObject");
+    retreivedObject = JSON.parse(userObject);
+    if (JSON.stringify(retreivedObject) == "[]") {
+      $(".result-desc").text("Oops!  We can't seem to find the email you're looking for. Want to try again?");
+    } else {
+      $(".username").append(
+        retreivedObject.first_name + " " + retreivedObject.last_name
+      );
+      $(".user-description").append(retreivedObject.description);
+      $(".address").append("<p>" + retreivedObject.address + "</p>");
+      $(".email").append("<p>" + retreivedObject.email + "</p>");
 
-            for (const phone_number in retreivedObject.phone_numbers) {
-                $(".phone-num").append("<p>" + `${retreivedObject.phone_numbers[phone_number]}` + "</p>")
-            }
+      for (const phone_number in retreivedObject.phone_numbers) {
+        $(".phone-num").append(
+          "<p>" + `${retreivedObject.phone_numbers[phone_number]}` + "</p>"
+        );
+      }
 
-            for (const relative in retreivedObject.relatives) {
-                $(".relatives").append("<p>" + `${retreivedObject.relatives[relative]}` + "</p>")
-            }
-        }
+      for (const relative in retreivedObject.relatives) {
+        $(".relatives").append(
+          "<p>" + `${retreivedObject.relatives[relative]}` + "</p>"
+        );
+      }
+
+      $(".result-card").show()
     }
+  }
 }
